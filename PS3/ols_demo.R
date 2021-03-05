6f1d6340b5b24f4f2741cf97797e433da221c13e
isaackleislemurphy <isaackleislemurphy@outlook.com>
2021-03-04 22:39:27 -0800

    adding to OLS demo

--- PS3/ols_demo.R
+++ PS3/ols_demo.R
@@ -1,12 +1,16 @@
 library(dplyr)
 library(glmnet)
+library(glmnetUtils)
 source("ingest.R")
 source("constants.R")
 source("utils.R")
 
 training_data = read.csv("./data/training_data_processed.csv") %>%
-  select(-X, -X.1) # comment this out if necessary
+  select(-X, -X.1) %>% # comment this out if necessary
   configure_folds()
+
+
+# Example 1: Straightaway Fit ---------------------------------------------
 
 # then wrap this in a loop/grid of hyperparameters
 lapply(1:length(training_data), function(i){
@@ -16,10 +20,47 @@ lapply(1:length(training_data), function(i){
   result = fit_predict(
     train_df=train_df,
     predict_df=predict_df,
-    predict_func=predict,
     scale_cols=CONT_COLNAMES,
     formula = as.formula("response~.")
   );
   result
 }) -> lm_example
+
+
+# Example 2: Tuning L1/L2 -------------------------------------------------
+
+grid_lambda = expand.grid(
+  "alpha" = c(1e-1, 1e-2, 1e-3, 1e-4)
+)
+
+lapply(1:nrow(grid_lambda), function(j){
+  cat(' - tune number: ', j, '/', nrow(grid_lambda), '\n')
+  # extract the hyperparams for this iteration
+  a = grid_lambda$alpha[j]
+  # iterate over folds
+  lapply(1:length(training_data), function(i){
+    # extract training and prediction data for that fold
+    train_df = training_data[[i]][[1]] %>%
+      select_at(c(CONT_COLNAMES, "response"));
+    predict_df = training_data[[i]][[2]];
+    # fit and predict for fold
+    result = fit_predict(
+      train_df=train_df, # fit_and_predict() will scale for us
+      predict_df=predict_df, # predict on this (we'll probably just use only the "next" entry, but can't hurt to predict for all)
+      # predict using glmnet formula
+      model_func=glmnetUtils:::glmnet.formula, # used to fit model
+      # predict.glmnet outputs one prediction for each lambda, so each shall be a column
+      predict_func = function(fit, newdata){bind_cols(newdata, data.frame(glmnetUtils:::predict.glmnet.formula(fit, newdata=newdata)))},
+      # we will scale these continuous column names
+      scale_cols=CONT_COLNAMES,
+      # the formula to be used
+      formula=as.formula("response~."),
+      # mixing hyperparam; glmnet chooses a reasonable grid of alphas
+      alpha=a
+    ) 
+    result %>%
+      mutate(fold_idx = i) # for filtering purposes later
+  })  -> fold_result
+})
+
 
--- PS3/utils.R
+++ PS3/utils.R
@@ -4,10 +4,17 @@ library(dplyr)
 # Standard Scaler ---------------------------------------------------------
 
 StandardScaler <- function(){
+  #' Constructor for object that performs (x - u)/sd scaling
+  #' Similar to sklearn.preprocessing.StandardScaler
   list(scale=NULL, is_fit=F, cols=NULL)
 }
 
 scaler_fit <- function(scaler, df, cols=CONT_COLNAMES){
+  #' Fits scaler parameters to data and saves those parameters for later use
+  #' @param scaler: StandardScaler. A StandardScaler object
+  #' @param df: data.frame. A dataframe containing data for the scaler fit
+  #' @param cols: character[p]. A vector of column names in `df` to perform the scaling on.
+  #' @return : StandardScaler. A fitted StandardScaler object
   scaler$cols = cols
   scaler$scale = scale(df[, cols], center=T, scale=T)
   scaler$is_fit=T
@@ -15,6 +22,10 @@ scaler_fit <- function(scaler, df, cols=CONT_COLNAMES){
 }
 
 scaler_transform <- function(scaler, df){
+  #' Transforms data according to a StandardScaler having been fit
+  #' @param scaler: StandardScaler. A StandardScaler object
+  #' @param df: data.frame. A dataframe containing data to be transformed. Must contain columns `cols`.
+  #' @return : data.frame. A transformed/scaled dataframe
   df[, scaler$cols] = data.frame(
     scale(df[, scaler$cols],
           center=attr(scaler$scale, 'scaled:center'),
@@ -24,21 +35,30 @@ scaler_transform <- function(scaler, df){
   df
 }
 
-
 # Train/Predict within Fold -----------------------------------------------
 
 fit_predict <- function(train_df, 
                         predict_df, 
                         model_func=lm, 
-                        predict_func=predict,
+                        predict_func=function(mod, newdata){newdata$yhat=stats::predict(mod, newdata=newdata); newdata},
                         scale_cols=CONT_COLNAMES, 
                         ...){
+  #' A function that trains on a training df and subsequently predicts on a new dataframe.
+  #' Intended use is for ("fold", "dev") pairing.
+  #' @param train_df: data.frame. A dataframe of training data, all of `scale_cols` unscaled at this point.
+  #' @param predict_df: data.frame. A dataframe of prediction/dev data, all of `scale_cols` unscaled at this point.
+  #' @param model_func: function. A function, e.g. `stats::lm()`, used to fit the model.
+  #' @param predict_func: function. A wrapper around a prediction function, e.g. `stats::predict()`, used to predict from a fitted model
+  #' Some functions, such as predict.glmnet, may return a matrix of yhats; hence the wrapper
+  #' @param scale_cols: character[p]. A vector of continuous-valued columns to scale prior to fitting. 
+  #' @param ...: args passed to predict_func. 
+  #' @return : data.frame. The dataframe with fitted predictions
   scaler = StandardScaler()
   scaler = scaler_fit(scaler, df=train_df, cols=scale_cols)
   train_df_sc = scaler_transform(scaler, train_df)
   predict_df_sc = scaler_transform(scaler, predict_df)
   fit = model_func(data=train_df_sc, ...)
-  predict_df$yhat = predict(fit, newdata=predict_df_sc)
+  predict_df = predict_func(fit, newdata=predict_df_sc)
   predict_df
 }
 
